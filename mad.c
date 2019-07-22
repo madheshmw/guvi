605c49c6921ab9b878b80898ce980b283dc63f6a
madheshmw <53176052+madheshmw@users.noreply.github.com>
2019-07-22 16:18:21 +0530

    Add files via upload

---
+++ mad.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+
+    int n;
+
+    scanf("%d",&n);
+
+    if(n>0)
+  
+  {
+
+        printf("%d is positive",n);
+   
+     
+    }
+
+    else if(n<0)
+  
+  {
+
+
+        printf("%d is negative",n);
+
+    }
+
+    else
+ 
+   {
+ 
+       printf("is zero");
+
+    }
+
+    return 0;
+
+}
