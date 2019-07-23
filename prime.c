a2931f83e556c3eb6a3419b52463b81974c4af9a
madheshmw <53176052+madheshmw@users.noreply.github.com>
2019-07-23 14:53:34 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+        c++;
+        }
+    
+    }
+    if(c==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
